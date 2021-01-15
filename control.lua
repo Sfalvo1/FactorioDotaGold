97fe12bb046d7b8e01348599c935b25e7d105a62
Sfalvo <sfalvo1@gmail.com>
2021-01-14 23:50:55 -0500

    Removed debug command and fixed instrument and sound name in speaker menu

--- control.lua
+++ control.lua
@@ -1,21 +1,6 @@
 --"commands" access the LuaCommandProcessor
 --"add_command" takes in three params (name -> string, help -> string, function)
 
-function foo(tableIn) -- Event pass a table into the function
-	--game.get_player() returns a LuaPlayer object
-	local player = game.get_player(tableIn.player_index)
-	
-	
-	local color = {r=1, g=0, b=0, a=1}
-	
-	-- print a message to just the player who sent it
-	player.print("Hello there! This is a private message to: " ..player.name, color)
-	
-	--game.print() would print a message to all players -> LuaGameScript
-	game.print("This is a global messsage to all players")
-
-end
-
 commands.add_command("say_hello", "this is a help message", foo)
 
 function goldsound(tableIn)
--- data-updates.lua
+++ data-updates.lua
@@ -1,9 +1,7 @@
 instrument = table.insert(data.raw["programmable-speaker"]["programmable-speaker"].instruments, 2, {
-	name = "DoTA2",
+	name = "dota2-sounds",
 	notes =
 	{
-        { 
-		name = "last-hit",  sound = { filename = "__dotagold__/coins.ogg" }
-		},
+        { name = "last-hit",  sound = { filename = "__dotagold__/coins.ogg" }},
 	}
 })
